76b28eb531aa2228f7b4aeb9b7a17e5d5ec49ba9
Piotr Oleskiewicz <piotr@oleskiewi.cz>
2021-01-24 00:33:02 +0000

    set up documentation builder

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
 using Documenter, Polarisation, Polarisation.Jones
 
 makedocs(sitename = "Polarisation.jl", modules = [Polarisation], doctest = false)
+
+deploydocs(repo = "github.com/oleskiewicz/Polarisation.jl.git")
